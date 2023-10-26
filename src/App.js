d94ca11ddc4dc128db7cf19193ed2d736e010256
Priyangaj-17 <124904598+Priyangaj-17@users.noreply.github.com>
2023-10-26 10:54:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+// import { Cart } from "./pages/cart/cart";
+import { ShopContextProvider } from "./context/shop-context";
+import { Shop } from "./pages/Shop/Shop";
+import { Cart } from "./pages/Cart/Cart";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
